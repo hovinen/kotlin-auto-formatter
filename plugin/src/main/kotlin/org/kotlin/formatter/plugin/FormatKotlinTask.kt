6085ebda2910c29bfa8e09a0cfd17fbf24ffcf11
Bradford Hovinen <hovinen@gmail.com>
2020-05-01 17:27:58 +0200

    Organize imports

--- plugin/src/main/kotlin/org/kotlin/formatter/plugin/FormatKotlinTask.kt
+++ plugin/src/main/kotlin/org/kotlin/formatter/plugin/FormatKotlinTask.kt
@@ -1,11 +1,8 @@
 package org.kotlin.formatter.plugin
 
-import org.gradle.api.file.FileCollection
-import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.SourceTask
 import org.gradle.api.tasks.TaskAction
 import org.kotlin.formatter.KotlinFormatter
-import java.util.concurrent.Callable
 
 internal open class FormatKotlinTask : SourceTask() {
     @TaskAction
