39b02e7b9e32fb3a8e88ca3e77ad19029b0541c7
Bradford Hovinen <hovinen@gmail.com>
2020-05-03 20:56:08 +0200

    Port ClassScanner to NodePattern

--- formatter/src/main/kotlin/org/kotlin/formatter/scanning/ClassScanner.kt
+++ formatter/src/main/kotlin/org/kotlin/formatter/scanning/ClassScanner.kt
@@ -6,31 +6,25 @@ import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.psiUtil.children
 import org.kotlin.formatter.State
 import org.kotlin.formatter.Token
+import org.kotlin.formatter.scanning.nodepattern.nodePattern
 
 internal class ClassScanner(private val kotlinScanner: KotlinScanner): NodeScanner {
-    override fun scan(node: ASTNode, scannerState: ScannerState): List<Token> {
-        val children = node.children().toList()
-        val indexOfClassKeyword = children.indexOfFirst { it.elementType == KtTokens.CLASS_KEYWORD }
-        val indexOfClassBody = children.indexOfFirst { it.elementType == KtNodeTypes.CLASS_BODY }
-        val nodesBeforeClassKeyword = children.subList(0, indexOfClassKeyword)
-        val tokensBeforeClassKeyword = kotlinScanner.scanNodes(nodesBeforeClassKeyword, scannerState = ScannerState.BLOCK)
-        val nodesWithinClassDeclaration =
-            if (indexOfClassBody == -1) {
-                children.subList(indexOfClassKeyword, children.size)
-            } else {
-                children.subList(indexOfClassKeyword, indexOfClassBody)
-            }
-        val tokensWithinClassDeclaration = kotlinScanner.scanNodes(nodesWithinClassDeclaration, scannerState = ScannerState.STATEMENT)
-        val tokensOfBlock =
-            if (indexOfClassBody != -1) {
-                kotlinScanner.scanNodes(children.subList(indexOfClassBody, children.size), ScannerState.BLOCK)
-            } else {
-                listOf()
-            }
-        return listOf(
-            *tokensBeforeClassKeyword.toTypedArray(),
-            *inBeginEndBlock(tokensWithinClassDeclaration, State.CODE).toTypedArray(),
-            *tokensOfBlock.toTypedArray()
-        )
+    private val nodePattern = nodePattern {
+        zeroOrMore { anyNode() } andThen { nodes ->
+            kotlinScanner.scanNodes(nodes, scannerState = ScannerState.BLOCK)
+        }
+        exactlyOne {
+            nodeOfType(KtTokens.CLASS_KEYWORD)
+            oneOrMoreFrugal { anyNode() }
+        } andThen { nodes ->
+            inBeginEndBlock(kotlinScanner.scanNodes(nodes, scannerState = ScannerState.STATEMENT), State.CODE)
+        }
+        zeroOrOne { nodeOfType(KtNodeTypes.CLASS_BODY) } andThen { nodes ->
+            kotlinScanner.scanNodes(nodes, ScannerState.BLOCK)
+        }
+        end()
     }
+
+    override fun scan(node: ASTNode, scannerState: ScannerState): List<Token> =
+        nodePattern.matchSequence(node.children().asIterable())
 }
