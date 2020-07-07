26e9e00e61e0add7fbfd69f73a65741abbac1c7c
Bradford Hovinen <hovinen@gmail.com>
2020-07-07 21:51:42 +0200

    Bump referenced plugin version

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     groovy
     `java-gradle-plugin`
     id("com.gradle.plugin-publish") version "0.12.0"
-    id("tech.formatter-kt.formatter") version "0.4.4"
+    id("tech.formatter-kt.formatter") version "0.4.5"
 }
 
 group = "tech.formatter-kt"
